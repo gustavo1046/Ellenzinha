ce69a23ed6e5292154538f6e3a75b0ed08eb3a9c
Gustavo <gustavo1046@hotmail.com>
2024-02-05 16:46:01 -0300

    criando elemento ask

---
+++ Ellenzinha/src/app/Components/ask/ask.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ask',
+  standalone: true,
+  imports: [],
+  templateUrl: './ask.component.html',
+  styleUrl: './ask.component.scss'
+})
+export class AskComponent {
+
+}
--- Ellenzinha/src/app/Pages/principal/principal.component.ts
+++ Ellenzinha/src/app/Pages/principal/principal.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { LetterComponent } from '../../Components/letter/letter.component';
+import { AskComponent } from '../../Components/ask/ask.component';
 
 @Component({
     selector: 'app-principal',
     standalone: true,
     templateUrl: './principal.component.html',
     styleUrl: './principal.component.scss',
-    imports: [LetterComponent]
+    imports: [LetterComponent, AskComponent]
 })
 export class PrincipalComponent {
 
