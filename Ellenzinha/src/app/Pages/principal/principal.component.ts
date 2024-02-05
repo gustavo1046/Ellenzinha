46a94307d334363aa825dea2ae77a3bb40cd2f5c
Gustavo <gustavo1046@hotmail.com>
2024-02-05 12:04:55 -0300

    letter finalizado

--- Ellenzinha/src/app/Pages/principal/principal.component.ts
+++ Ellenzinha/src/app/Pages/principal/principal.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { LetterComponent } from '../../Components/letter/letter.component';
 
 @Component({
-  selector: 'app-principal',
-  standalone: true,
-  imports: [],
-  templateUrl: './principal.component.html',
-  styleUrl: './principal.component.scss'
+    selector: 'app-principal',
+    standalone: true,
+    templateUrl: './principal.component.html',
+    styleUrl: './principal.component.scss',
+    imports: [LetterComponent]
 })
 export class PrincipalComponent {
 
